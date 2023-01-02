295d4bdd46087f57458ed517cdba246302d97b89
McSam-py <ardisam9876@gmail.com>
2023-01-02 10:41:00 +0000

    wrote a function to read bytes of the input file

--- send.py
+++ send.py
@@ -7,3 +7,12 @@ import time
 lhost = sys.argv[1]
 lport = int(sys.argv[2])
 file_path = sys.argv[3]
+
+
+def get_image_bytes(path):
+    with open(path, "rb") as file_bytes_object:
+        file_bytes = file_bytes_object.read()
+        file_size = file_bytes_object.seek(0, os.SEEK_END)
+
+    file_name = path.split("/")[-1]
+    return file_bytes, file_size, file_name
